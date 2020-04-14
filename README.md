5e6390ed2ca8f3fc231725632a5aff4805a3bccd
ali <ali@iMac-de-Elodie.fritz.box>
2020-04-15 00:06:46 +0200

    first commit to master

---
+++ README.md
@@ -0,0 +1,1 @@
+## Backend Development nodeJS MangoDB expressJs and Mongoose
---
+++ app.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+const port = 3000;
+const hostname = "127.0.0.1";
+
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("site/index");
+});
+app.get("/about", (req, res) => {
+  res.render("site/about");
+});
+app.get("/blog", (req, res) => {
+  res.render("site/blog");
+});
+app.get("/contact", (req, res) => {
+  res.render("site/contact");
+});
+app.get("/login", (req, res) => {
+  res.render("site/login");
+});
+app.get("/register", (req, res) => {
+  res.render("site/register");
+});
+
+app.listen(port, hostname, () =>{ 
+  console.log(` Example app listening, http://${hostname}:${port}/`)
+});
+
+
+10. video sonu
